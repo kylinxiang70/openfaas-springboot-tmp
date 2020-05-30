f8b1788baf50a71da31abd52a0465259777d6175
kylinxiang <15884286060@163.com>
2020-05-30 17:09:46 +0800

    add Handler.java

--- template/springboot/src/main/java/com/kylin/faas/javatmp/JavaTmpApplication.java
+++ template/springboot/src/main/java/com/kylin/faas/javatmp/JavaTmpApplication.java
@@ -6,14 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
 @SpringBootApplication
 public class JavaTmpApplication {
-    @GetMapping("/**")
-    public ResponseEntity<String> print() {
-
-        return ResponseEntity.ok("aaaa");
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(JavaTmpApplication.class, args);
